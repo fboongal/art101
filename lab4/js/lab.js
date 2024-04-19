95af200806d3bc93a652e93ba46113031647f325
Franchesca Boongaling <fboongal@ucsc.edu>
2024-04-19 02:15:55 -0700

    completed lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,41 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - An implemention of the game Rock-Paper-Scissors
+// Author: Franchesca Boongaling & Ruby Hirsch
+// Date: 4/19/24
 
-// Constants
+/*
 
-// Functions
+Task 2 Outline: Rock-Paper-Scissors
+1. Print the board before each turn
+  a) Print PNG symbols of Rock, Paper, and Scissors in a row
+2. Accept user input from player
+  a) remove images of moves not picked by player
+3. Validate move
+  a) store whether player chose Rock, Paper, or Scissors
+  b) print image of chosen player move on left side of screen
+4. Choose random computer move
+  a) Randomize between Rock, Paper, and Scissors
+  b) store whether computer chose Rock, Paper, or Scissors
+  c) print image of chosen computer move on right side of screen
+5. Evaluate moves from player and computer
+  a) compare chosen player and user moves
+6. Test if win, lose, or draw condition
+  a) Player chooses Rock:
+    i. win if computer chose Scissors
+    ii. draw if computer chose Rock
+    iii. lose if computer chose Paper
+  b) Player chooses Paper:
+    i. win if computer chose Rock
+    ii.draw if computer chose Paper
+    iii. lose if computer chose Scissors
+  c) Player chooses Scissors:
+    i. win if computer chose Paper
+    ii. draw if computer chose Scissors
+    iii. lose if computer chose Rock
+7. Print on screen "You Win!", if player wins, "Draw!", if player draws, or "You Lose!", if player loses.
+8. Print "Try Again?" button on screen
+  a) if player click button, restart game.
+
+*/
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
