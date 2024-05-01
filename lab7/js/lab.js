574836c68ce8c98b586a2554fa65bd1688537404
Franchesca Boongaling <fboongal@ucsc.edu>
2024-04-30 21:08:06 -0700

    completed task 7!

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,3 +1,15 @@
 // index.js - Functions
 // Author: Franchesca Boongaling & Ruby Hirsch
 // Date: 4/30/24
+
+// define function 
+function getUserName() {
+    // prompt to ask user for their name
+    const userName = window.prompt("Ye name please?!");
+
+    // convert name into an array, sort the array, join the array and turn into string
+    return userName.split('').sort().join('');
+}
+
+// function output
+document.writeln("Ye NEW name: " , getUserName(),"<br>");
