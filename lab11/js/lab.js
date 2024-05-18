d9ef08eab4859d8fb5fb9c3a6b0b53e09923863a
Franchesca <fboongal@ucsc.edu>
2024-05-18 15:40:48 -0700

    started lab 11 tasks 1-3

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,3 +1,19 @@
-// index.js - Functions
+// index.js - Events & Forms
 // Author: Franchesca Boongaling & Ruby Hirsch
-// Date: 4/30/24
+// Date: 5/18/24
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+  }
+
+// click listener for button
+$("#submit").click(function(){
+    // get value of input field
+    const userName = $("#user-name").val();
+    // now let's sort it
+    userNameSorted = sortString(userName);
+    // append a new div to our output div
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
