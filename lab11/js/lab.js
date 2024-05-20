7839949b0bf8c11c414286d7e7da88f9ee230d4d
Franchesca <fboongal@ucsc.edu>
2024-05-19 18:53:43 -0700

    finished task X for lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,18 +2,50 @@
 // Author: Franchesca Boongaling & Ruby Hirsch
 // Date: 5/18/24
 
-// Sorts the characters of a string in alphabetical order.
 function sortString(inputString) {
-    // We have to convert our string to an array and back again to sort it
     return inputString.split('').sort().join('');
   }
 
 // click listener for button
-$("#submit").click(function(){
-    // get value of input field
-    const userName = $("#user-name").val();
-    // now let's sort it
-    userNameSorted = sortString(userName);
-    // append a new div to our output div
-    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+$("#sort").click(function(){
+  const userName = $("#name").val();
+  const colorName = $("#color").val();
+  const aniName = $("#animal").val();
+
+  userNameSorted = sortString(userName);
+  colorNameSorted = sortString(colorName);
+  aniNameSorted = sortString(aniName);
+
+  $("#output").append('<p>' + userNameSorted + '</p>');
+  $("#output").append('<p>' + colorNameSorted + '</p>');
+  $("#output").append('<p>' + aniNameSorted + '</p>');
 });
+
+
+
+// Task X:
+function anagram(inputString) {
+  const charArray = inputString.split('')
+   // Fisher-Yates (Knuth) shuffle algorithm that shuffles characters
+ for (let i = charArray.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [charArray[i], charArray[j]] = [charArray[j], charArray[i]]; }
+ const anagram = charArray.join('');
+ return anagram;
+}
+
+$("#anagram").click(function(){
+  const userName = $("#name").val();
+  const colorName = $("#color").val();
+  const aniName = $("#animal").val();
+  
+  userNameSorted = anagram(userName);
+  colorNameSorted = anagram(colorName);
+  aniNameSorted = anagram(aniName);
+  
+$("#output").append('<p>' + userNameSorted + '</p>');
+$("#output").append('<p>' + colorNameSorted + '</p>');
+$("#output").append('<p>' + aniNameSorted + '</p>');
+});
+
+ 
