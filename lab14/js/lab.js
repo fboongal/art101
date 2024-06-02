830177cd4918bc57657b92cb698ee1159ef329a4
Franchesca <fboongal@ucsc.edu>
2024-06-01 21:13:26 -0700

    finished lab 14!

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,3 +1,25 @@
-// index.js - Functions
+// index.js - Debugging Lab 8
 // Author: Franchesca Boongaling & Ruby Hirsch
-// Date: 4/30/24
+// Date: 5/31/24
+
+// create an array of random numbers
+let anArray = [1, 3, 22, 14, 9, 6]
+console.log("Here's your array: ", anArray)
+
+// create a named function that adds +3 to anArray
+function addNumber(x) {
+    return (x+3);
+}
+
+let addResults = anArray.map(addNumber)
+
+// create an anonymous function that subtracts -3 to anArray
+let minusResults = anArray.map(function(x) {
+    return (x-3);
+})
+
+$("#output1").append(anArray.join(", "));
+$("#output2").append(addResults.join(", "));
+$("#output3").append(minusResults.join(", "));
+
+$("#old").append(anArray).append(addResults).append(minusResults);
