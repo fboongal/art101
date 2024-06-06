4fd4d862b44c54ce4a512949b07f002b18cd3169
Franchesca <fboongal@ucsc.edu>
2024-06-05 17:59:36 -0700

    finished lab 15!

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,3 +1,22 @@
-// index.js - Functions
+// index.js - AJAX
 // Author: Franchesca Boongaling & Ruby Hirsch
-// Date: 4/30/24
+// Date: 6/3/24
+
+const URL = "https://yesno.wtf/api";
+
+$("#activate").click(function(){
+    let question = $("#input").val();
+    console.log(question);
+    $("#the-question").html("<p>Your Question: <span class='bold'>" + question + "</span></p>")
+
+    $.ajax({
+        url: URL,
+        type: "GET"
+    })
+    .done(function(data) {
+    
+        let yN =  data.answer;
+        $("#output").html("<p>Your Answer: <span class='bold'>" + yN + "</span></p>" + "<img class='center' src=" + data.image + ">");    
+    });
+})
+
