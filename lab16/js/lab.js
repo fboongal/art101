a86f02fe819310a059aa8fa54f8998790df29b9c
Franchesca <fboongal@ucsc.edu>
2024-06-07 05:49:36 -0700

    lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,3 +1,15 @@
-// index.js - Functions
-// Author: Franchesca Boongaling & Ruby Hirsch
-// Date: 4/30/24
+// index.js - Lab 16: JSON and APIs
+// Author: Franchesca Boongaling and Ruby Hirsch
+// Date: 6/6/24
+
+$.ajax({
+    dataType: "json", // Note: dataType should be lowercase "json"
+    success: function(data) {
+        console.log(data);
+        $('#response').html(`<p>${data.answer}</p><img src="${data.image}" alt="Comic">`);
+    },
+    error: function(jqXHR, textStatus, errorThrown) { 
+        console.error("Error:", textStatus, errorThrown);
+        $('#response').html('<p>Sorry, something went wrong. Please try again later.</p>');
+    }
+});
